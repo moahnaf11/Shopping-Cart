45b9e5df8ed3e73cbcc854232d168e2da15867af
Ahnaf <mohammadahnaf123@gmail.com>
2024-08-20 18:42:28 +0400

    Cache react fetch request
    Cached fetch request so it only fetches on the very first mount

--- src/components/Men.jsx
+++ src/components/Men.jsx
@@ -10,13 +10,13 @@ export function Men() {
     // quantity button handlers
     function handleQuantity(e, id, action="") {
         setQuantity((prev) => {
-            return prev.map(item => {
+            const newArray = prev.map(item => {
                 if (item.id === id) {
                     let newQuantity;
     
                     if (action === "plus") {
                         newQuantity = item.quantity + 1;
-                    } else if (action === "sub") {
+                    } else if (action === "minus") {
                         newQuantity = Math.max(item.quantity - 1, 0); // Prevent going below 0
                     } else {
                         newQuantity = Number(e.target.value, 10) || 0; // Convert to integer or default to 0
@@ -26,8 +26,8 @@ export function Men() {
                 }
                 return item;
             })
-
-
+            console.log(newArray);
+            return newArray;
         })
 
     }
@@ -35,15 +35,29 @@ export function Men() {
     // filter items by search
     function handleSearch(e) {
         if (e.target.value) {
-            setData(prev => (
-                prev.filter((item) => item.title.toLowerCase().startsWith(e.target.value.toLowerCase()))
-
-            ))
+            console.log(e.target.value)
+            setData(prev => {
+                const updatedArray = prev.filter((item) => item.title.toLowerCase().includes(e.target.value.toLowerCase()))
+                console.log(updatedArray);
+                return updatedArray;
+            })
 
         }   else {
             setData(fullData);
         }
 
+
+    }
+
+    // update inputValue of form input field
+    function handleValue(id) {
+        const inputValue = quantity.find(q => q.id === id);
+        return inputValue.quantity;
+    }
+
+    // submit handler for add to cart button
+    function handleSubmit(e) {
+        e.preventDefault();
 
     }
 
@@ -57,6 +71,7 @@ export function Men() {
             });
             if (response.ok) {
                 clothing = await response.json();
+                localStorage.setItem("clothing", JSON.stringify(clothing));
             }
 
             const response2 = await fetch("https://fakestoreapi.com/products/category/electronics", {
@@ -65,6 +80,7 @@ export function Men() {
             });
             if (response2.ok) {
                 electronics = await response2.json();
+                localStorage.setItem("electronics", JSON.stringify(electronics));
             }
 
             const alldata = [...clothing, ...electronics];
@@ -84,8 +100,29 @@ export function Men() {
             setLoading(false)
 
         }
-           
-        fetchMen();
+        
+        let clothing, electronics;
+        if (localStorage.getItem("clothing") && localStorage.getItem("electronics")) {
+            setLoading(false);
+            clothing = JSON.parse(localStorage.getItem("clothing"));
+            electronics = JSON.parse(localStorage.getItem("electronics"));
+            const alldata = [...clothing, ...electronics];
+            const idQuantity = alldata.map(item => (
+                {
+                    id: item.id,
+                    quantity: 0, 
+                }
+            ))
+            console.log(alldata);
+            console.log(idQuantity);
+
+            setData([...clothing, ...electronics]);
+            setFullData([...clothing, ...electronics]);
+            setQuantity(idQuantity);
+
+        }   else {
+            fetchMen();
+        }
 
 
     }, [])
@@ -93,7 +130,7 @@ export function Men() {
     return (
         <>
             {loading && <div className="loader">Loading...</div>}
-            <input onChange={(e) => handleSearch(e)}></input>
+            <input onChange={(e) => handleSearch(e)} placeholder="search"></input>
 
             {data.length > 0 && <div className="menitem">
                 {data.length > 0 &&
@@ -107,7 +144,7 @@ export function Men() {
                             <div><button>View</button></div>
                             <div className="quantity">
                                 <button className="add" onClick={(e) => handleQuantity(e, item.id, "plus")}>+</button>
-                                <form id="myform" action="" method="post"><input value={(e) => null} onChange={(e) => handleQuantity(e, item.id)} type="text" pattern="^(0|[1-9][0-9]*)$" /></form>
+                                <form onSubmit={handleSubmit} id="myform" action="" method="post"><input value={handleValue(item.id)} onChange={(e) => handleQuantity(e, item.id)} type="text" pattern="^(0|[1-9][0-9]*)$" /></form>
                                 <button className="sub"onClick={(e) => handleQuantity(e, item.id, "minus")}>-</button>
                             </div>
                             <button type="submit" form="myform">Add to cart</button>
--- src/components/Women.jsx
+++ src/components/Women.jsx
@@ -1,5 +1,157 @@
+import { useState, useEffect } from "react";
+
 export function Women() {
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    const [fullData, setFullData] = useState([]);
+    const [quantity, setQuantity] = useState([]);
+
+    // quantity button handlers
+    function handleQuantity(e, id, action="") {
+        setQuantity((prev) => {
+            const newArray = prev.map(item => {
+                if (item.id === id) {
+                    let newQuantity;
+    
+                    if (action === "plus") {
+                        newQuantity = item.quantity + 1;
+                    } else if (action === "minus") {
+                        newQuantity = Math.max(item.quantity - 1, 0); // Prevent going below 0
+                    } else {
+                        newQuantity = Number(e.target.value, 10) || 0; // Convert to integer or default to 0
+                    }
+                    console.log({...item, quantity: newQuantity });
+                    return { ...item, quantity: newQuantity };
+                }
+                return item;
+            })
+            console.log(newArray);
+            return newArray;
+        })
+
+    }
+
+    // filter items by search
+    function handleSearch(e) {
+        if (e.target.value) {
+            setData(prev => (
+                prev.filter((item) => item.title.toLowerCase().startsWith(e.target.value.toLowerCase()))
+
+            ))
+
+        }   else {
+            setData(fullData);
+        }
+
+
+    }
+
+    // update inputValue of form input field
+    function handleValue(id) {
+        const inputValue = quantity.find(q => q.id === id);
+        return inputValue.quantity;
+    }
+
+    // submit handler for add to cart button
+    function handleSubmit(e) {
+        e.preventDefault();
+
+    }
+
+
+    useEffect(() => {
+        async function fetchWomen() {
+            let women, jewelry;
+            const response = await fetch("https://fakestoreapi.com/products/category/women's clothing", {
+                mode: "cors",
+                method: "get"
+            });
+            if (response.ok) {
+                women = await response.json();
+                localStorage.setItem("women", JSON.stringify(women));
+            }
+
+            const response2 = await fetch("https://fakestoreapi.com/products/category/jewelery", {
+                mode: "cors",
+                method: "get"
+            });
+            if (response2.ok) {
+                jewelry = await response2.json();
+                localStorage.setItem("jewelry", JSON.stringify(jewelry));
+            }
+
+            const alldata = [...women, ...jewelry];
+            const idQuantity = alldata.map(item => (
+                {
+                    id: item.id,
+                    quantity: 0, 
+                }
+            ))
+            console.log(alldata);
+            console.log(idQuantity);
+
+            setData([...women, ...jewelry]);
+            setFullData([...women, ...jewelry]);
+            setQuantity(idQuantity);
+            
+            setLoading(false);
+
+        }
+        let women, jewelry;
+        if (localStorage.getItem("women") && localStorage.getItem("jewelry")) {
+            setLoading(false);
+            women = JSON.parse(localStorage.getItem("women"));
+            jewelry = JSON.parse(localStorage.getItem("jewelry"));
+            const alldata = [...women, ...jewelry];
+            const idQuantity = alldata.map(item => (
+                {
+                    id: item.id,
+                    quantity: 0, 
+                }
+            ))
+            console.log(alldata);
+            console.log(idQuantity);
+
+            setData([...women, ...jewelry]);
+            setFullData([...women, ...jewelry]);
+            setQuantity(idQuantity);
+
+        }   else {
+            fetchWomen();
+        }
+        
+
+
+    }, [])
+
+
     return (
-        null
+        <>
+            {loading && <div className="loader">Loading...</div>}
+            <input onChange={(e) => handleSearch(e)} placeholder="search"></input>
+
+            {data.length > 0 && <div className="womenitem">
+                {data.length > 0 &&
+                    data.map((item) => (
+                        <div className="itemcards" key={item.id}>
+                            <div className="title">{item.title}</div>
+                            <div className="img">
+                                <img src={item.image} alt="item image" />
+                            </div>
+                            <div className="price">${item.price}</div>
+                            <div><button>View</button></div>
+                            <div className="quantity">
+                                <button className="add" onClick={(e) => handleQuantity(e, item.id, "plus")}>+</button>
+                                <form onSubmit={handleSubmit} id="myform" action="" method="post"><input value={handleValue(item.id)} onChange={(e) => handleQuantity(e, item.id)} type="text" pattern="^(0|[1-9][0-9]*)$" /></form>
+                                <button className="sub"onClick={(e) => handleQuantity(e, item.id, "minus")}>-</button>
+                            </div>
+                            <button type="submit" form="myform">Add to cart</button>
+                        </div>
+                ))}
+
+
+            </div>}
+
+        </>
     )
 }
